eca32a51eda83934261d73f6a91d91393ab8a065
Javed <javedmannur93@gmail.com>
2024-01-12 09:34:04 +0530

    portfolio created

---
+++ page.js
@@ -0,0 +1,29 @@
+let type = new Typed(".changing-text", {
+    Strings: ['ANDROID DEVELOPMENT', 'UI DESIGNING', 'UX DESIGNING'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+})
+
+// const text = document.querySelector(".changing-text");
+
+// const textload = () => {
+//     setTimeout(() => {
+//         text.textContent = "WEB DEVELOPER";
+
+//     }, 1000);
+// }
+
+// setTimeout(() => {
+//     text.textContent = "ANDROID DEVELOPER";
+// }, 2000);
+
+// setTimeout(() => {
+//     text.textContent = "UI DESIGNER";
+// }, 4000);
+
+// setTimeout(() => {
+//     text.textContent = "UX DESIGNER";
+// }, 8000);
+
+// textload();
